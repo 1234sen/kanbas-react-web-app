f8d35c902ae1094d051c9c6ac6d289f7787a7d0d
1234sen <105305316+1234sen@users.noreply.github.com>
2025-04-01 12:34:55 +0800

    Update index.js

--- src/components/kambaz-node-server-app/index.js
+++ src/components/kambaz-node-server-app/index.js
@@ -46,7 +46,8 @@ app.use(session(sessionOptions));
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL,
+    // origin: process.env.NETLIFY_URL,
+    origin: 'https://eatwhite-a5.netlify.app',
     exposedHeaders: ['set-cookie'] // 新增暴露set-cookie头
   })
 );
