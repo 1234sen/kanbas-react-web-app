badb486d6ef187383b96e1e44f3837ea5432691b
1234sen <105305316+1234sen@users.noreply.github.com>
2025-04-13 11:12:36 +0800

    Update index.tsx

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -5,7 +5,7 @@ import { Row, Col, Card, Button } from "react-bootstrap";
 import * as db from "../Database";
 import { toggleShowAllCourses, enrollInCourse, unenrollFromCourse } from "../../reducers/enrollmentReducer";
 
-import ReactImage from "../assets/react.jpg"
+import ReactImage from "../assets/react1.jpg"
 
 export default function Dashboard({
     courses,
